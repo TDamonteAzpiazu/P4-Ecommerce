c3de145aac07dacfb1409303fd32d2ee0e186fb5
TDamonteAzpiazu <tobidamonte@gmail.com>
2024-05-21 20:41:58 -0300

    HW D2

---
+++ ecommerce-tdamonteazpiazu/src/Users/users.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@nestjs/common";
+
+
+@Injectable()
+export class UsersService {
+    getUsers(): string {
+        return 'Get all users';
+}
+}
--- ecommerce-tdamonteazpiazu/src/app.module.ts
+++ ecommerce-tdamonteazpiazu/src/app.module.ts
@@ -1,11 +1,19 @@
-import { Module, Global } from '@nestjs/common';
+import { Global, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './Users/users.module';
+import { ProductsModule } from './Products/products.module';
+import { AuthModule } from './Auth/auth.module';
+import { LoggerMiddleware } from './middleware/logger';
 
 @Global()
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('/');
+  }
+}
